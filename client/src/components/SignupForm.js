356de454d884849ece6965f661881a16bcaaa45e
Tyler Lawson <tylaw93@yahoo.com>
2021-10-08 11:35:48 -0500

    Replaced the addUser function

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import { ADD_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
+  const [addUser,{error}] = useMutation(ADD_USER);
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
@@ -29,6 +31,9 @@ const SignupForm = () => {
 
     try {
       const response = await createUser(userFormData);
+      const {data} = await addUser({variables:{...userFormData}})
+      console.log(data);
+      Auth.login(data.addUser.token);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
